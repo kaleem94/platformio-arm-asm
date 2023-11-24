ce75c3a88e23866a06a649b8f970c3d4947bd39f
Mohamed Kaleemuddin Salahuddin <kaleem94@users.noreply.github.com>
2023-11-24 21:14:04 +0100

    Update hello.s

--- examples/hello-world-asm/src/hello.s
+++ examples/hello-world-asm/src/hello.s
@@ -1,18 +1,16 @@
+.text            
 .global _start
-
-.section .text
-# STDIN - 0
-# STDOUT - 1
-# STDERR - 2
-
-
 _start:
-    mov r7, #0x4
     mov r0, #1
     ldr r1, =message
-    mov r2, #13
+    ldr r2, =len
+    mov r7, #4
     swi 0
 
-.section .data
-    message:
-    .ascii "Hello World\n"
+    mov r7, #1
+    swi 0
+
+.data
+message:
+    .asciz "hello world\n"
+len = .-message    
